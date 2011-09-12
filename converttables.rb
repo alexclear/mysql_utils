c755ac05b6289d8f722438f0b32a76e24a74620e
Alex Chistyakov <alexclear@gmail.com>
2011-09-12 23:06:48 +0400

    Fixing MySQL connection procedure, connections can't be injected for some reason

--- converttables.rb
+++ converttables.rb
@@ -2,31 +2,32 @@ require 'rubygems'
 require 'mysql'
 
 begin
-  Mysql.new('localhost', '', '', 'information_schema') do |con|
-    puts "Connected to the information_schema schema"
-    schemas = {}
-    con.query("SELECT table_name, engine, table_schema FROM information_schema.tables WHERE table_schema NOT LIKE 'mysql' AND table_schema NOT LIKE 'information_schema' AND engine LIKE 'MyISAM'").inject(schemas) do |schemas, row|
-      schemas[row[2]] = [] unless schemas[row[2]].is_a? Array
-      schemas[row[2]].push(row[0])
-    end
-    schemas.keys.each do |schema|
-      puts "Processing schema: #{schema}"
-      Mysql.new('localhost', '', '', schema) do |con_schema|
-        schemas[schema].each do |table|
-          puts "Processing table: #{table}"
-          begin
-            con_schema.query("ALTER TABLE #{table} ENGINE=InnoDB")
-          rescue Mysql::Error => e
-            puts "Error code: #{e.errno}"
-            puts "Error message: #{e.error}"
-            puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
-          end
-        end
+  con = Mysql.new('localhost', '', '', 'information_schema')
+  puts "Connected to the information_schema schema"
+  schemas = {}
+  con.query("SELECT table_name, engine, table_schema FROM information_schema.tables WHERE table_schema NOT LIKE 'mysql' AND table_schema NOT LIKE 'information_schema' AND engine LIKE 'MyISAM'").each do |row|
+    schemas[row[2]] = [] unless schemas[row[2]].is_a? Array
+    schemas[row[2]].push(row[0])
+  end
+  schemas.keys.each do |schema|
+    puts "Processing schema: #{schema}"
+    con_schema = Mysql.new('localhost', '', '', schema)
+    schemas[schema].each do |table|
+      puts "Processing table: #{table}"
+      begin
+        con_schema.query("ALTER TABLE #{table} ENGINE=InnoDB")
+      rescue Mysql::Error => e
+        puts "Error code: #{e.errno}"
+        puts "Error message: #{e.error}"
+        puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
       end
     end
+    con_schema.close if con_schema
   end
 rescue Mysql::Error => e
   puts "Error code: #{e.errno}"
   puts "Error message: #{e.error}"
   puts "Error SQLSTATE: #{e.sqlstate}" if e.respond_to?("sqlstate")
+ensure
+  con.close if con
 end
